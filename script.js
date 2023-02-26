1a4bc0805e2fdacb37f5d2c8432e58ceee21f4b9
nurakbarova <nurayakbarova76@gmail.com>
2023-02-26 18:28:10 +0400

    task

---
+++ script.js
@@ -0,0 +1,12 @@
+let saat;
+let odenis;
+let result=(saat*100)/n;
+if(n>=3&&n<=5){
+    console.Writeline(0,02)
+}
+else if(n>=5&&n<=8){
+    console.Writline(0,02+,0,05)
+}
+else{
+    console.Writeline(0,02++,0,05++,0,75 )
+}
